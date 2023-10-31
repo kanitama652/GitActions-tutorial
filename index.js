b68352c542f3500e431473b7fbfd2dc2a3365b3d
kanitama652 <137903541+kanitama652@users.noreply.github.com>
2023-10-31 11:23:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const serverlessExpress = require('@vendia/serverless-express')
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('AWS')
 })
 
 exports.handler = serverlessExpress({ app })
